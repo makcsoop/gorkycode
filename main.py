61ead44b7303a86ee0232e577e58880c2ccf97bf
makcsoop <makc.roslov@gmail.com>
2024-11-24 01:52:13 +0300

    add registration

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 from flask import Flask
-from scripts import note
+from scripts import *
 
-
+test = {"login": "user", "password" : "1234", "password2": "1234", "name": "user", "email":"213123"}
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
+    if len(test["login"]) and len(test["password"]):
+        if check_user(test["login"], test["password"]):
+            return "yes"
+        else:
+            return "not user"
+    else:
+        return "not"
     
-    return ""
+@app.route("/registration", methods=['GET', 'POST'])
+def registration():
+    if not (isvalid_login(test["login"])):
+        return "Уже есть такое пользователь"
+    if  isvalid_password(test["password"], test["password2"]) and isvalid_value(test["login"], test["password"], test["password2"]):
+        base, cursor = connect_base()
+        cursor.execute(
+            f"""INSERT INTO users (login, password, name, role, email) VALUES ('{test["login"]}', '{test["password"]}', '{test["name"]}', 1, '{test["email"]}')""")
+        base.commit()
+        base.close()
+        return "Успешно"
+    elif not (isvalid_password(test["password"],  test["password2"])):
+        return 'Пароли не совпадают'
+    else:
+        return 'Заполни все поля!!!!'
 
 if __name__ == '__main__':
     app.run(port=8000, host='localhost')
--- scripts.py
+++ scripts.py
@@ -1,2 +1,41 @@
-def note():
-    pass
+import sqlite3
+
+
+def connect_base():
+    base = sqlite3.connect('data/base.db')
+    cursor = base.cursor()
+    return base, cursor
+
+
+def check_user(login, password):
+    base, cursor = connect_base()
+    user = cursor.execute(
+        f"""SELECT * FROM users WHERE login = '{login}' AND password = '{password}'""").fetchall()
+    base.close()
+    if len(user) != 0:
+        return user[0][0]
+    return 0
+
+
+def isvalid_login(login):
+    base = sqlite3.connect('data/base.db')
+    cursor = base.cursor()
+    all_login = cursor.execute(f"""SELECT * FROM users WHERE login = '{login}'""").fetchall()
+    base.close()
+    if len(all_login) != 0:
+        return False
+
+    return True
+
+
+def isvalid_password(first, second):
+    if first == second:
+        return True
+    return False
+
+def isvalid_value(*value):
+    for i in value:
+        if len(i) == 0:
+            return False
+    else:
+        return True
